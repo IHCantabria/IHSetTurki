941497d50e5d88ea4e7d614d21575f739837f2fa
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-05-31 10:53:41 +0200

    Correcting writer bug

--- IHSetTurki/calibration_2.py
+++ IHSetTurki/calibration_2.py
@@ -193,17 +193,17 @@ class cal_Turki_2(object):
         self.EF = self.EF[ii:]
         self.time = self.time[ii:]
 
-        idx = np.where((self.time < self.start_date) | (self.time > self.end_date))
+        idx = np.where((self.time < self.start_date) | (self.time > self.end_date))[0]
         self.idx_validation = idx
 
-        idx = np.where((self.time >= self.start_date) & (self.time <= self.end_date))
+        idx = np.where((self.time >= self.start_date) & (self.time <= self.end_date))[0]
         self.idx_calibration = idx
         self.hb_splited = self.hb[idx]
         self.dirb_splited = self.dirb[idx]
         self.EF_splited = self.EF[idx]
         self.time_splited = self.time[idx]
 
-        idx = np.where((self.time_obs >= self.start_date) & (self.time_obs <= self.end_date))
+        idx = np.where((self.time_obs >= self.start_date) & (self.time_obs <= self.end_date))[0]
 
         self.Obs_splited = self.Obs[idx]
         self.time_obs_splited = self.time_obs[idx]
